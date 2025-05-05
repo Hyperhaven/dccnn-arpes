c63b0de3ffe587de462158250a2c629809b3c826
Hyperhaven <Hyperhaven@proton.me>
2025-05-05 17:22:43 +0200

    Create loss_function module

---
+++ modules/loss_functions/losses.py
@@ -0,0 +1,10 @@
+import torch.nn as nn
+from pytorch_msssim import MS_SSIM
+
+def loss_function(output, target, alpha):
+    mae = nn.L1Loss()(output, target)
+    msssim = sum(
+        1 - MS_SSIM(data_range=1.0, channel=1)(output[i:i+1], target[i:i+1])
+        for i in range(output.shape[0])
+    ) / output.shape[0]
+    return (1 - alpha) * mae + alpha * msssim
